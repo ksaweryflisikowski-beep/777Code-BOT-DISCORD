1948124ca4b1b6cca0d283617d821fa54553eba7
ksaweryflisikowski-beep <ksaweryflisikowski@gmail.com>
2026-08-27 23:27:11 +0200

    Refactor config.js to use environment variables
    Removed hardcoded configuration values and added comments for clarity.

--- config.js
+++ config.js
@@ -1,34 +1,10 @@
 require('dotenv').config();
 
-module.exports = { // NIE RUSZAĆ - USTAW TO W .ENV
+module.exports = {
+    // ==========================================
+    // DANE BOTA - USTAWIONE W BOT-HOSTING → ENV
+    // ==========================================
+
     TOKEN: process.env.TOKEN,
     CLIENT_ID: process.env.CLIENT_ID,
-    GUILD_ID: process.env.GUILD_ID,
-    
-    // Ustaw tutaj id kanałów
-    CHANNELS: {
-        TICKETS: '1494382864517697596',
-        WELCOME: '1495370548249890837',
-        VERIFICATION: '1494719799035695246',
-        PROPOSALS: '1494383451980173428'
-    },
-    
-    CATEGORIES: {
-        TICKETS: '1495376977580916816'
-    },
-    
-    ROLES: {
-        ADMIN: '1494717987855270089',
-        PLAYER: '1495371180234899476',
-        VERIFIED: '1494718998665760829'
-    },
-
-    BOT: {
-        NAME: 'KIXIMC.PL',
-        STATUS: 'dnd',
-        ACTIVITY_TYPE: 5,
-        FOOTER_TEXT: '© 2025 KIXIMC.PL'
-    },
-
-    // RESZTĘ CONFIG.JS ZOSTAW TAK JAK MASZ
-};
+    GUILD_ID:
